a5ded7e63bb02f85ff3a5c333bd18ad8a87e3297
jbrown <siftgrab@users.noreply.github.com>
2021-02-04 13:25:16 +0400

    Add files via upload

--- parse_evtx_tasks.py
+++ parse_evtx_tasks.py
@@ -14,9 +14,22 @@ from Evtx.Evtx import FileHeader
 from Evtx.Views import evtx_file_xml_view
 
 evtx_ids = {102,106,110,140,141,142,145,200,201,202,319}
-header = 'Date,EventID,EventDataName,ProcessID,ThreadID,Keywords,'\
-      'ActionName,TaskName,UserName,UserContext,Command,Path,Priority,'\
-      'ResultCode,TaskInstanceID,ProcessID,CurrentQuota,ErrorDescription'
+header = 'Date,EventID,EventDataName,ProcessID,ThreadID,ActionName,'\
+      'TaskName,UserName,UserContext,Command,Path,Priority,ResultCode,'\
+      'TaskInstanceID,ProcessID,CurrentQuota,ErrorDescription'
+
+event_data_names = ('ActionName',
+'TaskName',
+'UserName',
+'UserContext',
+'Command',
+'Path',
+'Priority',
+'ResultCode',
+'TaskInstanceID',
+'ProcessID',
+'CurrentQuota',
+'ErrorDescription')
 
 def main():
     parser = argparse.ArgumentParser(
@@ -32,19 +45,36 @@ def main():
             for xml, record in evtx_file_xml_view(fh):
                 soup = BeautifulSoup(xml, "lxml")
                 Date = soup.event.system.timecreated['systemtime']
+                Date = Date[:-7]
                 EventID = int(soup.event.system.eventid.string)
                 ProcessID = soup.event.system.execution['processid']
                 ThreadID = soup.event.system.execution['threadid']
                 EventDataName = soup.eventdata['name']
                 Keywords = soup.event.system.keywords.string
-                event_info = "%s,%s,%s,%s,%s,%s," % \
-                    (Date,
-                    EventID,
-                    EventDataName,
-                    ProcessID,
-                    ThreadID,
-                    Keywords)
-                
+                if EventID:
+                    event_info = "%s,%s,%s,%s,%s," % \
+                        (Date,
+                        EventID,
+                        EventDataName,
+                        ProcessID,
+                        ThreadID)
+ 
+                    try:
+                        event_data = {}
+                        for child in soup.eventdata.children:
+                            if type(child) is element.Tag:
+                                event_data[child['name']] = ' '.join(child.text.split())
+                        event_data_result = []
+                        for value in event_data_names:
+                            result = event_data.get(value)
+                            if result is None:
+                                result = ''
+                            event_data_result.append(result)
+                    except:
+                        pass
+                        
+                    print((event_info) + ','.join(map(str,event_data_result)))
+'''                    
                 event_data = {}
                 for child in soup.eventdata.children:
                     if type(child) is element.Tag:
@@ -77,6 +107,6 @@ def main():
                         ErrorDescription)
                         
                         print(event_info + event_data_values)
-                        
+ '''                       
 if __name__ == "__main__":
     main()
